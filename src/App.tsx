8c52693f34fb15f6177ea83d298606cecc7ceba3
Victor M <victomc.586@gmail.com>
2023-04-15 22:38:24 -0500

    Install ChakraUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return <Button colorScheme='blue'>Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}>
+      <GridItem area='nav' bg='coral'>
+        Nav
+      </GridItem>
+      <Show above='lg'>
+        <GridItem area='aside' bg='blue'>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bg='red'>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
