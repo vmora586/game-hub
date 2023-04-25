e6d93140aefdc94a44edd25677e670e989bad52f
Victor M <victomc.586@gmail.com>
2023-04-24 21:48:17 -0500

    Add game card skeleton

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,5 @@
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import PlatformIconsList from "./PlatformIconsList";
 import CriticScore from "./CriticScore";
 import getCroppedImage from "../services/image-url";
@@ -9,7 +9,7 @@ interface GameCardProps {
 }
 function GameCard({ game }: GameCardProps) {
   return (
-    <Card borderRadius={10} overflow={"hidden"}>
+    <Card width='250px' borderRadius={10} overflow={"hidden"}>
       <Image src={getCroppedImage(game.background_image)}></Image>
       <CardBody>
         <Heading fontSize='2xl'>{game.name}</Heading>
