55099a240310ab0485679cb66b5d08e899e8baae
Victor M <victomc.586@gmail.com>
2023-04-24 20:55:24 -0500

    Displaying critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import PlatformIconsList from "./PlatformIconsList";
+import CriticScore from "./CriticScore";
 
 interface GameCardProps {
   game: Game;
@@ -11,10 +12,13 @@ function GameCard({ game }: GameCardProps) {
       <Image src={game.background_image}></Image>
       <CardBody>
         <Heading fontSize='2xl'>{game.name}</Heading>
-        <PlatformIconsList
-          key={game.id}
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
+        <HStack justifyContent='space-between'>
+          <PlatformIconsList
+            key={game.id}
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
