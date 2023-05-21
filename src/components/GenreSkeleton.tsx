2705c49180efd5f5e352e639486f4874f0034946
Victor M <victomc.586@gmail.com>
2023-05-21 17:42:28 -0500

    refactor genre skeleton component

--- src/components/GenreSkeleton.tsx
+++ src/components/GenreSkeleton.tsx
@@ -1,23 +1,25 @@
 import {
+  Card,
+  CardBody,
   HStack,
+  Image,
   List,
   ListItem,
   Skeleton,
+  SkeletonCircle,
   SkeletonText,
   Text,
 } from "@chakra-ui/react";
 
 const GenreSkeleton = () => {
-  const skeletons = [1, 2, 3, 4, 5, 6];
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   return (
     <List>
       {skeletons.map((skeleton) => (
         <ListItem key={skeleton} paddingY='5px'>
-          <HStack>
-            <Skeleton height='32px' borderRadius={8} />
-            <Text>
-              <SkeletonText />
-            </Text>
+          <HStack spacing={2} mt={4}>
+            <SkeletonCircle />
+            <Skeleton height='20px' width='60px'></Skeleton>
           </HStack>
         </ListItem>
       ))}
