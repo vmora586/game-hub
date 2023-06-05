7f5f608556fb525a1bc61508ab9426cb30700635
Victor Mora <vmora@peopleready.com>
2023-06-05 13:18:27 -0500

    Ship static data

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,13 +1,5 @@
-import useData from "./useData";
+import platforms from "../data/Platforms";
 
-interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
-
-const usePlatforms = () => {
-  return useData<Platform>("/platforms/lists/parents");
-};
+const usePlatforms = () => ({data:platforms, error:null});
 
 export default usePlatforms;
